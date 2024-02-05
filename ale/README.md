9833756f7f25e5454e2211229a2ee77d8701dfb6
Kostas Georgiadis <kongeor@gmail.com>
2024-02-05 21:44:33 +0200

    wip

---
+++ ale/README.md
@@ -0,0 +1,16 @@
+# 
+
+## Prerequisites
+
+- [Clojure](https://clojure.org/guides/install_clojure)
+- [pyenv](https://github.com/pyenv/pyenv?tab=readme-ov-file#installation)
+
+
+## Install
+
+pyenv local
+
+pip install ale-py
+
+export PYTHONHOME="/Users/luca/.pyenv/versions/3.7.6"
+
--- ale/src/ale/core.clj
+++ ale/src/ale/core.clj
@@ -1,5 +1,6 @@
 (ns ale.core
-  (:require [libpython-clj2.require :refer [require-python]]
+  (:require [clojure.java.io :as io]
+            [libpython-clj2.require :refer [require-python]]
             [libpython-clj2.python :as py :refer [py. py.. py.-]]
             [chickn.core :as chickn]
             [chickn.util :as util]
@@ -9,11 +10,13 @@
   'ale_py
   '(ale_py ALEInterface))
 
-(defn play-game [moves & {:keys [random-seed display-screen? sound? record? recording-dir] :or {random-seed 42
-                                                                                                display-screen? false
-                                                                                                sound? false
-                                                                                                record? false
-                                                                                                recording-dir "resources/recording"}}]
+(def recording-dir "resources/recording")
+
+(defn play-game [rom moves & {:keys [random-seed display-screen? sound? record? recording-dir] :or {random-seed 42
+                                                                                                    display-screen? false
+                                                                                                    sound? false
+                                                                                                    record? false
+                                                                                                    recording-dir recording-dir}}]
   (let [ale (ale_py/ALEInterface)]
     (py. ale setInt "random_seed" random-seed)
     (when display-screen?
@@ -24,8 +27,9 @@
       (py. ale setString "record_screen_dir" recording-dir)
       (py. ale setString "record_sound_filename" (str recording-dir "/sound.wav"))
       (py. ale setInt "fragsize" 64))
-    (py. ale loadROM "resources/roms/demon_attack.bin")
+    (py. ale loadROM (str "resources/roms/" rom))
     (let [legal-moves (py. ale getLegalActionSet)]
+      #_(println "legal moves" legal-moves)
       (loop [i      0
              reward 0]
         (if (or
@@ -38,45 +42,68 @@
             (recur (inc i) (+ reward reward'))))))))
 
 (defn rand-move []
-  (rand-int 6 #_18))
+  ;; 0 noop
+  ;; 1 fire
+  ;; 2 up
+  ;; 3 right
+  ;; 4 left
+  ;; 5 down
+  ;; ...
+  (rand-nth [0 1 3 4] #_18))
 
 (comment
-  (play-game (repeatedly 1000 rand-move) {:record? false
-                                          :display-screen? true
-                                          }))
+  (play-game (repeatedly 10 rand-move) {:record? true
+                                        :display-screen? false}))
 
-(def ga-cfg
-  #:chickn.core{:chromo-gen   #(repeatedly 5000 rand-move)
-                :pop-size     40
-                :elitism-rate 0.1
-                :terminated?  util/noop
+(defn make-config [rom]
+  #:chickn.core{:chromo-gen   #(repeatedly 1000 rand-move)
+                :population-size     40
+                :solved?      util/noop
+                :fitness      (partial play-game rom)
                 :monitor      util/noop
-                :fitness      play-game
                 :comparator   chickn/higher-is-better
                 :reporter     util/simple-printer
                 :parallel     false
-                :selectors    [#:chickn.selectors{:type        :chickn.selectors/best
-                                                  :elit        true
-                                                  :rate        0.1
-                                                  :random-func rand}
-                               #:chickn.selectors{:type        :chickn.selectors/tournament
-                                                  :rate        0.5
-                                                  :random-func rand
-                                                  :tour-size   8}]
-                :operators    [#:chickn.operators{:type         :chickn.operators/cut-crossover
-                                                  :rate         0.5
-                                                  :pointcuts    1
-                                                  :random-point math/rnd-index
-                                                  :random-func  rand
-                                                  :rand-nth     rand-nth}
-                               #:chickn.operators{:type          :chickn.operators/rand-mutation
-                                                  :rate          0.5
-                                                  :random-func   rand
-                                                  :mutation-func rand-move}]})
+                :selector    #:chickn.selector{:type        :chickn.selector/tournament
+                                                 :rate        0.5
+                                                 :random-func rand
+                                                 :tour-size   8}
+                :crossover    #:chickn.crossover{:type         :chickn.crossover/cut-crossover
+                                                 :rate         0.5
+                                                 :pointcuts    1
+                                                 :random-point math/rnd-index
+                                                 :random-func  rand
+                                                 :rand-nth     rand-nth}
+                :mutation     #:chickn.mutation{:type          :chickn.mutation/rand-mutation
+                                                :rate          0.5
+                                                :random-func   rand
+                                                :mutation-func rand-move}
+                :reinsertion     #:chickn.reinsertion{:type :chickn.reinsertion/elitist
+                                                      :rate 0.1}})
+
+;; TODO ensure
+
+(defn reset-fs []
+  (let [f (java.io.File. recording-dir)]
+    (.mkdir f))
+  (doseq [f (file-seq (java.io.File. recording-dir))]
+    (when (.isFile f)
+      (.delete f))))
 
 (comment
-  (let [genotype (chickn/init ga-cfg)]
-    (-> (chickn/evolve ga-cfg genotype 50)
-      :genotype
-      :best-chromo
-      (play-game {:record? true :sound? true}))))
+  (reset-fs))
+
+(comment
+  (let [rom "demon_attack.bin"
+        ga-cfg (make-config rom)
+        population (chickn/init ga-cfg)
+        iterations 2
+        ;; evolve and find the best
+        best-chromosome (:best-chromosome (chickn/evolve ga-cfg population iterations))]
+    ;; delete existing files
+    (reset-fs)
+    ;; play the game again and record
+    (play-game rom best-chromosome {:record? true :display-screen? false :sound? true})
+    ;;
+    #_(clojure.java.shell/sh)
+    ))
